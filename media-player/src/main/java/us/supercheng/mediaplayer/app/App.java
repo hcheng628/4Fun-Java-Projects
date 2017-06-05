75781b76e74ee31d0bde15dd115b5c21c52c21b7
superhcheng <chenghongyu628@gmail.com>
2017-06-05 18:13:49 -0400

    setting up menuitems

--- media-player/src/main/java/us/supercheng/mediaplayer/app/App.java
+++ media-player/src/main/java/us/supercheng/mediaplayer/app/App.java
@@ -3,12 +3,38 @@ package us.supercheng.mediaplayer.app;/**
  */
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+
 public class App extends Application {
+    final private String App_Name = "Light Weight Media Player";
+    final private int App_Default_Width = 1280;
+    final private int App_Default_Height = 720;
+    final private String FXML_Folder_Path = "/FXMLs/";
+    final private String FXML_Extension = ".fxml";
+
+    private FileChooser fileChooser;
+    private MenuBar menuBar;
+    private Menu menuFile;
+
+    public App(){
+        this.fileChooser = new FileChooser();
+
+        this.menuBar = new MenuBar();
+        this.menuFile = new Menu("Open File");
+        this.menuBar.getMenus().add(this.menuFile);
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -16,9 +42,22 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(this.getClass().getResource("/FXMLs/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        // Parent root = FXMLLoader.load(this.getClass().getResource(FXML_Folder_Path +"AppController" + FXML_Extension));
+        EventHandler<ActionEvent> action = new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent event){
+                event.get
+            }
+        };
+        this.menuFile.setOnAction(action);
+
+
+        primaryStage.setTitle(App_Name);
+        BorderPane root = new BorderPane();
+        Pane pane = new Pane();
+        pane.getChildren().add(this.menuBar);
+        root.setTop(pane);
+        Scene scene = new Scene(root, App_Default_Width, App_Default_Height);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
