42a69e454b5288c2b34801a4439cca3efd42b9aa
superhcheng <chenghongyu628@gmail.com>
2017-06-05 17:07:23 -0400

    setup maven for javaFX

---
+++ media-player/src/main/java/us/supercheng/mediaplayer/app/App.java
@@ -0,0 +1,24 @@
+package us.supercheng.mediaplayer.app;/**
+ * Created by cl799honchen on 6/5/2017.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(this.getClass().getResource("/FXMLs/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
---
+++ media-player/src/main/java/us/supercheng/mediaplayer/app/AppController.java
@@ -0,0 +1,8 @@
+package us.supercheng.mediaplayer.app;
+
+/**
+ * Created by cl799honchen on 6/5/2017.
+ */
+public class AppController {
+
+}
