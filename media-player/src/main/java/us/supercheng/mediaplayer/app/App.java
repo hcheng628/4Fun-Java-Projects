e8fac821b23b0fb5ba1a939f361d57a44e0638bc
superhcheng <chenghongyu628@gmail.com>
2017-06-07 12:03:20 -0400

    implement media progress bar 2 way bindings and updates its label, volume control and updates its label

--- media-player/src/main/java/us/supercheng/mediaplayer/app/App.java
+++ media-player/src/main/java/us/supercheng/mediaplayer/app/App.java
@@ -3,6 +3,9 @@ package us.supercheng.mediaplayer.app;/**
  */
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -15,11 +18,14 @@ import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 import java.io.File;
 import java.net.URLEncoder;
 
@@ -50,32 +56,57 @@ public class App extends Application {
     private Menu menuFile;
     private MenuItem menuItemOpenFile;
     private MenuItem menuItemOpenURL;
+    private MenuItem menuItemCloseApp;
 
+    private Label mediaVolumeSliderLabel;
     private Slider mediaVolumeSlider;
+
+    private Label mediaProgressSliderLabel;
     private Slider mediaProgressSlider;
 
     private Button mediaPlayBtn;
 
+    private VBox topMenuContainer;
+    private ToolBar bottomToolBar;
+
 
 
     public App(){
+        this.mediaVolumeSliderLabel = new Label("Volume: 100%");
         this.mediaVolumeSlider = new Slider();
         this.mediaVolumeSlider.setMin(0.0);
-        this.mediaVolumeSlider.setMax(200.0);
+        this.mediaVolumeSlider.setMax(100.0);
+        this.mediaVolumeSlider.setValue(100.0);
+        this.mediaVolumeSlider.setMaxWidth(60.0);
 
+        this.mediaProgressSliderLabel = new Label("");
         this.mediaProgressSlider = new Slider();
 
         this.mediaPlayBtn = new Button(Media_Play_Btn_Playing_Stage);
 
         this.menuBar = new MenuBar();
-        this.menuFile = new Menu("Open");
+        this.menuFile = new Menu("Menu");
         this.menuItemOpenFile = new MenuItem("File Path");
         this.menuItemOpenURL = new MenuItem("File URL");
+        this.menuItemCloseApp = new MenuItem("Exit");
 
         this.menuFile.getItems().add(this.menuItemOpenFile);
         this.menuFile.getItems().add(this.menuItemOpenURL);
+        this.menuFile.getItems().add(this.menuItemCloseApp);
+
         this.rootPanel = new BorderPane();
         this.menuBar.getMenus().add(this.menuFile);
+
+        this.bottomToolBar = new ToolBar(
+                this.mediaPlayBtn,
+                this.mediaProgressSlider,
+                this.mediaProgressSliderLabel,
+                this.mediaVolumeSlider,
+                this.mediaVolumeSliderLabel
+        );
+
+        this.topMenuContainer = new VBox();
+
 
     }
 
@@ -87,14 +118,29 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle(App_Name);
         Scene scene = new Scene(this.rootPanel, App_Default_Width, App_Default_Height);
-        Pane paneTop = new Pane();
         Pane paneBottom = new Pane();
+
+        // Close App
+        this.menuItemCloseApp.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                ((Stage)menuBar.getScene().getWindow()).close();
+            }
+        });
 
         // Load media from Disk
         this.menuItemOpenFile.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 try{
-                    player = new MediaPlayer(new Media("file:///" + new FileChooser().showOpenDialog((Stage) menuBar.getScene().getWindow()).getAbsolutePath().replace("\\", "/")));
+                    // String tempStr = new FileChooser().showOpenDialog((Stage) menuBar.getScene().getWindow()).toURI().toString();
+                    // System.out.println(tempStr);
+                    player = new MediaPlayer(new Media("file:/C:/Users/cl799honchen/Downloads/SampleAudio_0.7mb.mp3"));
+
+                    player.currentTimeProperty().addListener(new InvalidationListener() {
+                        public void invalidated(Observable observable) {
+                            updateMediaProgress();
+                        }
+                    });
+
                     rootPanel.setCenter(new MediaView(player));
                 }catch(Exception ex){
                     ex.printStackTrace();   // Dont care
@@ -116,23 +162,45 @@ public class App extends Application {
             }
         });
 
-        // Progress Bar
-        this.mediaProgressSlider.valueProperty().addListener(new ChangeListener<Number>() {
+        this.mediaVolumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                System.out.println("Old Val: " + oldValue + " New Val: " + newValue);
+                // System.out.println("Old Val: " + oldValue + " New Val: " + newValue);
+                player.setVolume(newValue.doubleValue()/100.00);
+                mediaVolumeSliderLabel.setText("Volume: " + (int)Math.round(player.getVolume()* 100)  + "%");
             }
         });
 
-        paneTop.getChildren().add(this.menuBar);
+        // Progress Bar
+        this.mediaProgressSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                // System.out.println("Old Val: " + oldValue + " New Val: " + newValue);
+                if(mediaProgressSlider.isValueChanging()){
+                    player.seek(new Duration(player.getMedia().getDuration().toMillis() * mediaProgressSlider.getValue() / 100));
+                }
+            }
+        });
 
-        paneBottom.getChildren().add(this.mediaPlayBtn);
-        paneBottom.getChildren().add(this.mediaProgressSlider);
-        paneBottom.getChildren().add(this.mediaVolumeSlider);
 
-        this.rootPanel.setTop(paneTop);
+        this.topMenuContainer.getChildren().add(this.menuBar);
+
+        paneBottom.getChildren().add(this.bottomToolBar);
+
+        this.rootPanel.setTop(this.topMenuContainer);
         this.rootPanel.setBottom(paneBottom);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    // Update Progress Bar
+    protected void updateMediaProgress(){
+        Platform.runLater(new Runnable(){
+            public void run() {
+                mediaProgressSlider.setValue(player.getCurrentTime().toMillis() / player.getMedia().getDuration().toMillis() * 100);
+                mediaProgressSliderLabel.setText(((int)(player.getCurrentTime().toMillis()/3600000) % 24) + ":" + ((int)(player.getCurrentTime().toMillis()/60000) % 60) +
+                        ":" + ((int)(player.getCurrentTime().toMillis()/1000) % 60) + " / " + ((int)(player.getMedia().getDuration().toMillis()/3600000)%24) + ":" +
+                        ((int)(player.getMedia().getDuration().toMillis()/60000)%60) + ":" + ((int)(player.getMedia().getDuration().toMillis()/1000)%60));
+            }
+        });
     }
 }
